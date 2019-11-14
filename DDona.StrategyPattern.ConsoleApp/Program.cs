3b10f73628dc8799707351f06f2f87b0521c4362
Diego Doná <diedona@gmail.com>
2019-11-14 17:16:40 -0300

    display

--- DDona.StrategyPattern.ConsoleApp/Program.cs
+++ DDona.StrategyPattern.ConsoleApp/Program.cs
@@ -12,7 +12,7 @@ namespace DDona.StrategyPattern.ConsoleApp
             PersonService personService = new PersonService(new PeopleNameSorter(SortDirection.Asc));
             foreach (var person in personService.GetPeople())
             {
-                Console.WriteLine($"{person.Id} - {person.FullName} - {person.BirthDate.ToShortDateString()}");
+                Console.WriteLine($"{person.Id} - {person.FullName.PadRight(30,' ')} - {person.BirthDate.ToShortDateString()}");
             }
 
             Console.WriteLine();
@@ -22,7 +22,7 @@ namespace DDona.StrategyPattern.ConsoleApp
             personService.SetStrategy(new PeopleBirthDate(SortDirection.Desc));
             foreach (var person in personService.GetPeople())
             {
-                Console.WriteLine($"{person.Id} - {person.FullName} - {person.BirthDate.ToShortDateString()}");
+                Console.WriteLine($"{person.Id} - {person.FullName.PadRight(30, ' ')} - {person.BirthDate.ToShortDateString()}");
             }
         }
     }
