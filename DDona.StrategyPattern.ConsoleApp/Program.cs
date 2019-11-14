c8d6bdef2131ecb090320b5b18ea389d6562685f
Diego Doná <diedona@gmail.com>
2019-11-14 17:10:14 -0300

    implementing

--- DDona.StrategyPattern.ConsoleApp/Program.cs
+++ DDona.StrategyPattern.ConsoleApp/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DDona.StrategyPattern.ConsoleApp.Sorters;
+using DDona.StrategyPattern.Domain.Enum;
+using DDona.StrategyPattern.Services;
+using System;
 
 namespace DDona.StrategyPattern.ConsoleApp
 {
@@ -6,7 +9,21 @@ namespace DDona.StrategyPattern.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PersonService personService = new PersonService(new PeopleNameSorter(SortDirection.Asc));
+            foreach (var person in personService.GetPeople())
+            {
+                Console.WriteLine($"{person.Id} - {person.FullName} - {person.BirthDate.ToShortDateString()}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine();
+
+            personService.SetStrategy(new PeopleBirthDate(SortDirection.Desc));
+            foreach (var person in personService.GetPeople())
+            {
+                Console.WriteLine($"{person.Id} - {person.FullName} - {person.BirthDate.ToShortDateString()}");
+            }
         }
     }
 }
