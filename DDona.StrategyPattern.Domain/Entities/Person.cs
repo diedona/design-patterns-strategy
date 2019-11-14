13ff2d02a084119eb795d2c1e927160e8b6873d0
Diego Doná <diedona@gmail.com>
2019-11-14 16:31:14 -0300

    domain

---
+++ DDona.StrategyPattern.Domain/Entities/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDona.StrategyPattern.Domain.Entities
+{
+    public class Person
+    {
+        public Guid Id { get; private set; }
+        public string FullName { get; private set; }
+        public DateTime BirthDate { get; private set; }
+
+        public Person(Guid id, string fullName, DateTime birthDate)
+        {
+            Id = id;
+            FullName = fullName;
+            BirthDate = birthDate;
+
+            if(this.BirthDate > DateTime.Now)
+            {
+                throw new ArgumentOutOfRangeException("BirthDate", "Future dates not allowed");
+            }
+        }
+    }
+}
